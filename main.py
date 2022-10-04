44058d84b4fae29925838c29e56d37186789edc9
Jose Augusto Sanchez <pepe80@gmail.com>
2022-10-04 12:42:07 -0300

    change script to generate bartaco files for update prices

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import csv
 import shutil
 from tempfile import NamedTemporaryFile
 
-ORIGINAL = 'original.csv'
+ORIGINAL = 'juicer file output 9-29.csv'
 ORIGINAL_KEY_FIELDNAME = 'Price Seq'
-ORIGINAL_VAL_FIELDNAME = 'Price'
+ORIGINAL_VAL_FIELDNAME = 'Price 1'
 
-CHANGES = 'changes.csv'
+CHANGES = 'price_change_THURSDAY_17hr.csv'
 
 
 DICT_KEY_FIELDNAME = 'Price Seq'
-DICT_VAL_FIELDNAME = 'Price'
+DICT_VAL_FIELDNAME = 'Price 1'
 
 OUTPUT_FILE = 'my.csv'
+
 
 tempfile = NamedTemporaryFile(mode='w', delete=False)
 
@@ -21,9 +22,10 @@ memory_dict = dict()
 # Create a memory dictionary where the item key is price_sec
 # and value is the updated price value
 with open(CHANGES, newline='', encoding='utf8') as changes:
-    reader = csv.DictReader(changes, quoting=csv.QUOTE_NONNUMERIC)
-    for row in reader:
+    reader = csv.DictReader(changes)    
+    for row in reader:        
         memory_dict[int(row[DICT_KEY_FIELDNAME])] = row[DICT_VAL_FIELDNAME]
+
 
 with open(
     ORIGINAL,
@@ -31,14 +33,21 @@ with open(
     newline='',
     encoding='utf-8',
 ) as csvfile:
-    reader = csv.DictReader(csvfile, quoting=csv.QUOTE_MINIMAL)
+    reader = csv.DictReader(csvfile)    
     writer = csv.DictWriter(
         tempfile,
         fieldnames=reader.fieldnames,
-        quoting=csv.QUOTE_MINIMAL,
+        quoting=csv.QUOTE_NONE,
+        quotechar='/'
     )
-
-    for row in reader:
+    writer.writeheader()
+    writer.fieldnames.append(None)
+    for row in reader: 
+                
+        for field in ('Item name','Price Group','Price Tier', 'Cross Reference 1','Cross Reference 2','Combo Group'):    
+            row[field]='"'+row[field]+'"'
+                   
+        
         row[ORIGINAL_VAL_FIELDNAME] = memory_dict[int(row[ORIGINAL_KEY_FIELDNAME])]
         writer.writerow(row)
 
--- test.py
+++ test.py
@@ -1,20 +1,35 @@
 # importing the pandas library
-from csv import QUOTE_NONNUMERIC
+from csv import QUOTE_NONNUMERIC, QUOTE_MINIMAL
+from operator import index
 from numpy import int_
 import pandas as pd
 
 # reading the csv file
 
-ORIGINAL = 'original.csv'
-PRICE_CHANGE = 'changes.csv'
+ORIGINAL = 'juicer file output 9-29.csv'
+PRICE_CHANGE = 'price_change_THURSDAY_17hr.csv'
 COL_NAME = 'Price Seq'
-COL_NEW_PRICE = 'Price'
+COL_NEW_PRICE = 'Price 1'
 
 df1 = pd.read_csv(ORIGINAL)
-df1_columns = df1.columns
-df2 = pd.read_csv(PRICE_CHANGE, usecols=[COL_NAME, COL_NEW_PRICE])
+print(df1.head())
 
-df1.set_index(COL_NAME, inplace=True)
-df1.update(df2.set_index(COL_NAME))
-df1.reset_index(inplace=True)
-df1[df1_columns].to_csv("test_final.csv", index=False, quotechar='"', quoting=QUOTE_NONNUMERIC, lineterminator='\r\n')
+df1_columns = df1.columns
+df2 = pd.read_csv(PRICE_CHANGE, usecols=[COL_NAME, COL_NEW_PRICE], index_col=[COL_NAME])
+print('df1', df1['Price Seq'].head())
+print('df2', df2['Price Seq'].head())
+df2['Price Seq']=df2['Price Seq'].astype('float64')
+df3=df1.merge(df2, on='Price Seq', how='inner')
+
+df3['Price 1_x']=df3['Price 1_y']
+df3.drop(['Price 1_y'], axis=1, inplace=True)
+#df3=df3.drop('Price 1_y')
+df3.rename(columns={'Price 1_x': 'Price 1'}, inplace=True)
+#df3.rename('Price 1_x', 'Price 1')
+print(df3.shape)
+
+#df1.set_index(COL_NAME, inplace=True)
+#print(df1)
+#df1.update(df2.set_index(COL_NAME))
+#df1.reset_index(inplace=True)
+#df1[df1_columns].to_csv("test_final.csv", quoting=QUOTE_NONNUMERIC, index=False, line_terminator='\r\n')
